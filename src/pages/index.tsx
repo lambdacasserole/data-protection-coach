fc9c54c1054e4a9baa9dc789d7aad3a1bc59b8e5
Saul Johnson <saul.a.johnson@gmail.com>
2023-12-01 10:54:47 +0100

    Fully comment the important bits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,98 @@
+import { PromptType } from "@prisma/client";
 import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { Button } from "~/components/Button";
 import { Favicon } from "~/components/Favicon";
+import { Spinner } from "~/components/Spinner";
+import { TextField } from "~/components/TextField";
+import { api } from "~/utils/api";
 
 /**
- * The website homepage.
+ * The website frontend (the page the user interacts with).
  */
 export default function Home() {
+  // Keep a record of our currently selected prompt type (which button is checked on the frontend).
+  const [promptType, setPromptType] = useState<PromptType>(
+    PromptType.BUSINESS_SECTOR,
+  );
+
+  // Store our prompt (in the text box).
+  const [prompt, setPrompt] = useState<string>("");
+
+  // Store the result we get back from ChatGPT.
+  const [result, setResult] = useState<string | null>(null);
+
+  // This just lets us talk to our web application's backend.
+  const submitQuery = api.query.submitQuery.useMutation({
+    onSuccess: (result) => setResult(result),
+  });
+
+  // This is the contents of the page itself.
   return (
     <>
       <Head>
-        <title>Blank Application</title>
-        <meta name="description" content="Blank application" />
+        <title>Data Protection Coach</title>
+        <meta
+          name="description"
+          content="Data Protection Coach: Your coach on staying GDPR compliant in the workplace!"
+        />
+        <link rel="icon" href="/favicon.ico" />
         <Favicon />
       </Head>
       <main className="flex min-h-screen flex-col items-center">
         <div className="container flex flex-col items-center justify-center gap-6 pt-12">
-          <div className="mt-12 rounded border border-neutral-400 bg-neutral-50 px-6 py-5 font-mono text-neutral-400">
-            There&apos;s nothing here.
+          {/* Logo. */}
+          <Image src="/logo.svg" height={128} width={306} alt="Logo" />
+
+          {/* Introductory text. */}
+          <div>What do I need to know regarding the GDPR and my...</div>
+
+          {/* Our row of prompt type buttons (business sector, database contents and job role). */}
+          <div className="flex gap-6">
+            <Button
+              onClick={() => setPromptType(PromptType.BUSINESS_SECTOR)}
+              text="Business sector"
+              checked={promptType === PromptType.BUSINESS_SECTOR}
+            />
+            <Button
+              onClick={() => setPromptType(PromptType.DATABASE_CONTENTS)}
+              text="Database contents"
+              checked={promptType === PromptType.DATABASE_CONTENTS}
+            />
+            <Button
+              onClick={() => setPromptType(PromptType.JOB_ROLE)}
+              text="Job role"
+              checked={promptType === PromptType.JOB_ROLE}
+            />
           </div>
+
+          {/* Some more text. */}
+          <div>Which is...</div>
+
+          {/* The text field into which our user enters their prompt. */}
+          <TextField onChange={(e) => setPrompt(e.target.value)} />
+
+          {/* The submit button! */}
+          <Button
+            onClick={() => submitQuery.mutate({ promptType, prompt })}
+            text="What do I need to know?"
+            loading={submitQuery.isLoading}
+          />
+
+          {/* The result box. We only show this once the submit button is clicked. */}
+          {(result !== null || submitQuery.isLoading) && (
+            <div className="w-[512px] rounded border border-neutral-300 bg-neutral-50 p-4">
+              {submitQuery.isLoading ? (
+                <div className="flex w-full place-content-center items-center">
+                  {/* Show the loading spinner if we're still waiting for a response. */}
+                  <Spinner />
+                </div>
+              ) : (
+                result
+              )}
+            </div>
+          )}
         </div>
       </main>
     </>
--- src/server/api/routers/query.ts
+++ src/server/api/routers/query.ts
@@ -1,3 +1,52 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { PromptType } from "@prisma/client";
+import { z } from "zod";
 
-export const queryRouter = createTRPCRouter({});
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { askChatGpt } from "~/utils/askChatGpt";
+
+/**
+ * This is the website backend. Here's where we do the data processing and talking to ChatGPT.
+ */
+export const queryRouter = createTRPCRouter({
+  submitQuery: publicProcedure
+    .input(
+      z.object({
+        // These are sent from our web application frontend.
+        prompt: z.string(), // The prompt our user entered in the text box.
+        promptType: z.nativeEnum(PromptType), // The type of prompt they had selected (business sector, database contents, or job role).
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+
+      // Simply ask ChatGPT a question depending on the prompt type we had selected. Include the prompt from the frontend.
+      let chatGptResponse = "";
+      switch (input.promptType) {
+        case PromptType.BUSINESS_SECTOR:
+          chatGptResponse = await askChatGpt(
+            `Summarise in 2 sentences or fewer everything I need to know about GDPR compliance as a business working in the following sector: ${input.prompt}`,
+          );
+          break;
+        case PromptType.DATABASE_CONTENTS:
+          chatGptResponse = await askChatGpt(
+            `Summarise in 2 sentences or fewer everything I need to know about GDPR compliance as an employee working with a database containing the following customer information: ${input.prompt}`,
+          );
+          break;
+        case PromptType.JOB_ROLE:
+          chatGptResponse = await askChatGpt(
+            `Summarise in 2 sentences or fewer everything I need to know about GDPR compliance as a worker with the following role: ${input.prompt}`,
+          );
+          break;
+      }
+
+      // Put our prompt and the response from ChatGPT in the database (important for analytics!).
+      await ctx.db.query.create({
+        data: {
+          prompt: input.prompt,
+          response: chatGptResponse,
+          promptType: input.promptType,
+        },
+      });
+
+      return chatGptResponse;
+    }),
+});
